9846a5f9c98aa7d78eec8f0628bc81c9e0536089
cezary-gawronski <idetepowiec@gmail.com>
2020-05-24 05:18:28 +0200

    update js mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,23 @@
-const hamburger = document.querySelector('.hamburger--js');
-console.log(hamburger);
-
-hamburger.addEventListener('click', () => {
-    const nav = document.querySelector('.navigation__list--js');
-    nav.classList.toggle('navigation__list--open');
-})
-
+// const hamburger = document.querySelector('.hamburger--js');
+// console.log(hamburger);
+// hamburger.addEventListener('click', () => {
+//     const nav = document.querySelector('.navigation__list--js');
+//     nav.classList.toggle('navigation__list--open');
+// });
 
 
 
+(() => {
+    const button = document.querySelector('.hamburger--js');
+    const menu = document.querySelector('.navigation__list');
+    const logoLink = document.querySelector('.navigation__logo');
 
-// navigation__list--js
+    const toggleClass = () => {
+        menu.classList.toggle('navigation__list--open');
+    };
 
-// .navigation__list--open
+    button.addEventListener("click", toggleClass);
+    menu.addEventListener("click", toggleClass);
+    logoLink.addEventListener("click", toggleClass);
+
+})();
